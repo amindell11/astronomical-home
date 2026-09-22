d691f4cc0b591cfe35b61cf106dbb04a8ab3f34a
Arye Mindell <arimindell@gmail.com>
2026-09-21 22:48:43 -0700

    style: drop blank line left by enableDebugLogs deletion

--- src/Asteroids3D/Assets/Scripts/Ships/Movement/MovementController.cs
+++ src/Asteroids3D/Assets/Scripts/Ships/Movement/MovementController.cs
@@ -10,7 +10,6 @@ namespace Ships.Movement
     [DefaultExecutionOrder(50)]
     public class MovementController : MonoBehaviour, IPilot
     {
-
         [Header("Movement Gizmos")]
         public float movementGizmoScale = 3f;
 
